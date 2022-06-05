5884d91c3e16851c891e57f7696f59f65aa2602e
Anna Kapustina <asdfg7408@gmail.com>
2022-06-05 11:38:50 +0300

    Небольшие изменения в структуре файлов и папок

--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,8 @@
 import pytest
 import time
-from .pages.product_page import ProductPage
-from .pages.basket_page import BasketPage
-from .pages.login_page import LoginPage
+from pages.product_page import ProductPage
+from pages.basket_page import BasketPage
+from pages.login_page import LoginPage
 
 
 class TestUserAddToBasketFromProductPage:
@@ -33,17 +33,18 @@ class TestUserAddToBasketFromProductPage:
         page.should_be_cart_sum()
 
 
+# @pytest.mark.parametrize('num',
+#                        ["0", "1", "2", "3", "4", "5", "6", pytest.param("7", marks=pytest.mark.xfail), "8", "9"])
 @pytest.mark.need_review
-@pytest.mark.parametrize('num',
-                         ["0", "1", "2", "3", "4", "5", "6", pytest.param("7", marks=pytest.mark.xfail), "8", "9"])
-def test_guest_can_add_product_to_basket(browser, num):
-    # link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+def test_guest_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     # link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{num}"
+    # link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{num}"
     page = ProductPage(browser, link)
     page.open()
     page.should_not_be_success_message()
     page.add_cart()
+    page.solve_quiz_and_get_code()
     page.should_be_book_name()
     page.should_be_cart_sum()
 
